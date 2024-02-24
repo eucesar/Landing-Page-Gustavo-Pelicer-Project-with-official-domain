e838e754fe28d20b0626513bc6be40af907f1c22
Cesar Iglesias <cesaribneto.job@gmail.com>
2024-02-24 02:25:34 -0300

    excluindo-erro

--- Gustavo Pelicer/projeto/assets/js/main.js
+++
@@ -1,5 +0,0 @@
-const preloaderWrapper = document.querySelector('.preloader-wrapper');
-
-window.addEventListener('load', function() {
-    preloaderWrapper.classList.add('fade-out-animation');
-});
